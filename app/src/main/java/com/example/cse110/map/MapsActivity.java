ca74c12a0d7570401624f68771b9e564987240a7
Kevin Pham <kphamngu@ucsd.edu>
2015-10-29 16:13:18 -0700

    combined my stuff with andrew, no map though on mine

--- app/src/main/java/com/example/cse110/map/MapsActivity.java
+++ app/src/main/java/com/example/cse110/map/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.example.cse110.map;
 
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +26,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button toReport = (Button) findViewById(R.id.toReport);
+        toReport.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MapsActivity.this, MainActivity2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 
 
