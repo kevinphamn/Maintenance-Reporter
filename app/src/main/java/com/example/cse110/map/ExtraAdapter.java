339b04628f79f4e8f6ee9a24b54d4e73af49b87a
Justin Harris <jmh027@ucsd.edu>
2015-11-30 10:19:51 -0800

    MainActivity3Activity - Added new button with functionality to decrement counter/delete object from Parse.

--- app/src/main/java/com/example/cse110/map/ExtraAdapter.java
+++ app/src/main/java/com/example/cse110/map/ExtraAdapter.java
@@ -1,13 +1,19 @@
 package com.example.cse110.map;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.GpsStatus;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,13 +21,17 @@ import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class ExtraAdapter extends ArrayAdapter<ParseObject> {
 
     private ArrayList<ParseObject> myObjects;
+    private ArrayList<Boolean> canPress = new ArrayList<>();
     private ParseFile photo;
     Bitmap bmp = null;
 
@@ -30,7 +40,7 @@ public class ExtraAdapter extends ArrayAdapter<ParseObject> {
         this.myObjects = objects;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent)
+    public View getView(final int position, View convertView, ViewGroup parent)
     {
         View emptyView = convertView;
 
@@ -39,13 +49,15 @@ public class ExtraAdapter extends ArrayAdapter<ParseObject> {
             emptyView = theInflater.inflate(R.layout.lister_layout, null);
         }
 
-        ParseObject myObject = myObjects.get(position);
+        final ParseObject myObject = myObjects.get(position);
 
         if (myObject != null) {
             if(myObject.getInt("count") > -3) {
                 TextView theRoom = (TextView) emptyView.findViewById(R.id.theRoomNumber);
                 TextView theProblem = (TextView) emptyView.findViewById(R.id.theProblem);
                 final ImageView theImage = (ImageView) emptyView.findViewById(R.id.thePicture);
+                Button theInvalidButton = (Button) emptyView.findViewById(R.id.theInvalidButton);
+                canPress.add(true);
 
                 theRoom.setText("Room Number: ");
                 theRoom.append((String) myObject.get("RoomNumber"));
@@ -74,9 +86,59 @@ public class ExtraAdapter extends ArrayAdapter<ParseObject> {
                     // Set image to the pic_unavailable drawable resource
                     theImage.setImageResource(R.drawable.pic_unavailable);
                 }
+
+                theInvalidButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (canPress.get(position)) {
+                            ParseQuery<ParseObject> query = ParseQuery.getQuery("DataPoint");
+                            ParseObject object;
+
+                            ConnectivityManager cm = (ConnectivityManager) getContext().
+                                    getSystemService(Context.CONNECTIVITY_SERVICE);
+                            NetworkInfo info = cm.getActiveNetworkInfo();
+                            if (info == null || !info.isConnectedOrConnecting()) {
+                                createAlert("Unable to Submit", "Unable to submit input. Try again later.");
+                                return;
+                            }
+
+                            try {
+                                object = query.get(myObject.getObjectId());
+                            } catch (ParseException e) {
+                                createAlert("Unable to Submit", "Unable to submit input. Try again later.");
+                                return;
+                            }
+
+                            object.put("count", (int) myObject.get("count") - 1);
+                            if(object.getInt("count") <= -3) {
+                                try {
+                                    object.delete();
+                                } catch (ParseException el) {}
+                            }
+                            object.saveEventually();
+                            createAlert("Submission Success", "Input Successful");
+                            canPress.set(position, false);
+                        }
+                        else{
+                            createAlert("Feedback Submitted", "Your feedback has already been submitted");
+                        }
+                    }
+                });
             }
         }
 
         return emptyView;
+    }
+
+    private void createAlert(String title, String message){
+        AlertDialog dialog = new AlertDialog.Builder(getContext())
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
     }
 }
